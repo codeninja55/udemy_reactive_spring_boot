af934c71d0a9d65c7b66923b6b2145e9f0d9d1fa
Andrew Che <andrew@codeninja55.me>
2019-12-23 16:24:43 +1100

    Adding module for learning reactive

---
+++ learnreactive/src/main/kotlin/me/codeninja55/learnreactive/LearnreactiveApplication.kt
@@ -0,0 +1,11 @@
+package me.codeninja55.learnreactive
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearnreactiveApplication
+
+fun main(args: Array<String>) {
+	runApplication<LearnreactiveApplication>(*args)
+}
